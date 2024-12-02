58678378f936ab2ffaa623a930f875eddb885404
angel10arcila <131724057+angel10arcila@users.noreply.github.com>
2024-12-01 23:49:59 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+ 
 // Configuración de medios predefinidos
 const mediaFiles = [
 
@@ -28,7 +28,7 @@ const mediaFiles = [
      
      {
          name: "Una Comedía Romántica - Ángel José Arcila Parra",
-         url: "https://raw.githubusercontent.com/angel10arcila/angeljosearcilaparra/refs/heads/main/Comedia Romantica v10 - LyM de Ángel José Arcila Parra.mp3",
+         url: "https://raw.githubusercontent.com/angel10arcila/angeljosearcilaparra/refs/heads/main/Comedia  Romantica v10  - LyM de Ángel José Arcila Parra.mp3",
          type: "audio"
      },
      
