18e95c7e8c7a7aa6c3f9c4f5f4996a75f14328b1
angel10arcila <131724057+angel10arcila@users.noreply.github.com>
2025-04-26 16:50:31 -0400

    Update script.js

--- script.js
+++ script.js
@@ -64,7 +64,7 @@ const mediaFiles = [
   
     { 
          name: "Me enamoré de tí - Ángel José Arcila Parra",
-         url: "https://raw.githubusercontent.com/angel10arcila/angeljosearcilaparra/refs/heads/main/Crece cómo un árbol V7 Letra de Ángel José Arcila Parra.mp3",
+         url: "https://raw.githubusercontent.com/angel10arcila/angeljosearcilaparra/refs/heads/main/Me enamoré de tí - V1 Letra y Música de Ángel José Arcila Parra .mp3",
          type: "audio"
     },
     {
