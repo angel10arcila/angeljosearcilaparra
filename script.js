f5d478293a4c1e916ece76ba4e7181cac6523bfe
angel10arcila <131724057+angel10arcila@users.noreply.github.com>
2025-04-26 16:36:54 -0400

    Update script.js

--- script.js
+++ script.js
@@ -50,6 +50,13 @@ const mediaFiles = [
          url: "https://raw.githubusercontent.com/angel10arcila/angeljosearcilaparra/refs/heads/main/Noche rara - Letra de Ángel José Arcila Parra.mp3",
          type: "audio"
     },
+
+ 
+    { 
+         name: "Inspirame con tú arte - Ángel José Arcila Parra",
+         url: "https://raw.githubusercontent.com/angel10arcila/angeljosearcilaparra/refs/heads/main/Noche rara - Letra de Ángel José Arcila Parra.mp3",
+         type: "audio"
+    },
     {
          name: "The clock - Ángel José Arcila Parra",
          url: "https://raw.githubusercontent.com/angel10arcila/letraseningles/refs/heads/main/The Clock 1 Letra y Música de Ángel José Arcila Parra .mp3",
