ea865453126f64a67a7a8cbe29184705d75660d6
angel10arcila <131724057+angel10arcila@users.noreply.github.com>
2025-04-26 16:49:32 -0400

    Update script.js

--- script.js
+++ script.js
@@ -55,10 +55,15 @@ const mediaFiles = [
          name: "Inspirame con tú arte - Ángel José Arcila Parra",
          url: "https://raw.githubusercontent.com/angel10arcila/angeljosearcilaparra/refs/heads/main/Inspirame con tú arte - LyM de Ángel José Arcila Parra.mp3",
          type: "audio"
+    }, 
+    { 
+         name: "Crece cómo un árbol  - Ángel José Arcila Parra",
+         url: "https://raw.githubusercontent.com/angel10arcila/angeljosearcilaparra/refs/heads/main/Crece cómo un árbol V7 Letra de Ángel José Arcila Parra.mp3",
+         type: "audio"
     },
   
     { 
-         name: "Crece cómo un árbol  - Ángel José Arcila Parra",
+         name: "Me enamoré de tí - Ángel José Arcila Parra",
          url: "https://raw.githubusercontent.com/angel10arcila/angeljosearcilaparra/refs/heads/main/Crece cómo un árbol V7 Letra de Ángel José Arcila Parra.mp3",
          type: "audio"
     },
