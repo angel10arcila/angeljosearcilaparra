7115cea57d08e795f6c2c4416999a6b92406ce1a
angel10arcila <131724057+angel10arcila@users.noreply.github.com>
2025-07-04 10:46:04 -0400

    Update script.js

--- script.js
+++ script.js
@@ -13,12 +13,12 @@ const mediaFiles = [
         type: "Tv"
     }, 
     {
-        name: "Mlb 2 - BAL vs ATL ( 7:10 PM/ET )",
+        name: "Mlb 2 - BAL vs ATL ( 7:15 PM/ET )",
         url: "https://edge1caster.pro/hls/bzagLHzach19.m3u8?st=LTRMXm7UDsG4aHjx9AVAoIwvaE9EH2RpZhvuV-CaHRc&e=1751579628",
         type: "Tv"
     },
     {
-        name: "Mlb 3 - HOU - LAD ( 7:15 PM/ET )",
+        name: "Mlb 3 - HOU - LAD ( 9:10 PM/ET )",
         url: "https://edge1caster.pro/hls/jkzahzadach20.m3u8?st=zlaCmsOu1IlA0pAti1VsQV7zu155JpY436ul8O1ZI-4&e=1751579437",
         type: "Tv"
     }, 
